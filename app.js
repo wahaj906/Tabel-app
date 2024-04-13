95d1ac9ac3b077d4cb717af86978f9aa4eaacc11
Wahaj khan <154734596+wahaj906@users.noreply.github.com>
2024-04-13 11:35:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const form = document.querySelector("#taleGenrator");
-const input = document.querySelector("#tableNumber");
-const result = document.querySelector("#result");
+let form = document.querySelector("#taleGenrator");
+let input = document.querySelector("#tableNumber");
+let result = document.querySelector("#result");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const tableOf = input.value;
+  let tableOf = input.value;
   result.innerHTML = "";
 
-  for (i = 1; i <= 12; i++) {
+  for (let i = 1; i <= 12; i++) {
     const message = `${tableOf} x ${i} = ${tableOf * i}`;
 
-    result.innerHTML += `<p>${message}</p><hr>`;
+    result.innerHTML += `${message} <br>`;
   }
 });
